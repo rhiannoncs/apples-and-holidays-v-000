21af4526573bcf0313c2f0dcc63f11a8b4b11ed8
Rhiannon Shoults <rhiannon.csw@gmail.com>
2018-01-26 19:49:20 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -63,8 +63,8 @@ def all_supplies_in_holidays(holiday_hash)
       if holiday.to_s.include? "_"
         holiday_array_pt1 = holiday.to_s.split("_")
         holiday_array_pt2 = holiday_array_pt1.collect{|string| string.capitalize}
-        puts "   #{holiday_array_pt2.join(" ")}: #{supply_array.join(", ")}"
-      else puts "   #{holiday.to_s.capitalize}: #{supply_array.join(", ")}"
+        puts "  #{holiday_array_pt2.join(" ")}: #{supply_array.join(", ")}"
+      else puts " #{holiday.to_s.capitalize}: #{supply_array.join(", ")}"
       end
     end
   end
